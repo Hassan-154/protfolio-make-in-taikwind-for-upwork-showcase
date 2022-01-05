b191cc9c52b9e48cc76f2c8542239818e8e3944e
Hassan-154 <hassanbajwa154@gmail.com>
2022-01-05 17:43:43 +0500

    upaing my code

---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className='hero-section min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500'>
+<div class="hero-content flex h-screen justify-center items-center">
+    <div class="text-center">
+        <h1 class="text-3xl">Welcome</h1>
+        <p class="text-xl">Sub text</p>
+        <button type="button" class="cursor pointer h-16 w-40 rounded-full text-green-500 font-semibold text-2xl border-4 border-green-500">Hire Me</button>
+    </div>
+  </div>
+        </div>
+    )
+}
+
+export default HeroSection
