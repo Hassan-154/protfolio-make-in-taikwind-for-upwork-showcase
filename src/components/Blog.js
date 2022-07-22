75a52bcc2c0989f407cba78ca33fa5120e48ab8c
Your Name <you@example.com>
2022-07-22 23:23:30 +0500

    I change the blog post picture

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -35,8 +35,8 @@ function Blog() {
           <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -44,9 +44,9 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
@@ -71,8 +71,8 @@ function Blog() {
   <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -80,9 +80,9 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
@@ -104,8 +104,8 @@ function Blog() {
       <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -113,9 +113,9 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
@@ -141,8 +141,8 @@ function Blog() {
   <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -150,9 +150,9 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
@@ -180,8 +180,8 @@ function Blog() {
   <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -189,9 +189,9 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
@@ -215,8 +215,8 @@ function Blog() {
   <div class="break-inside mb-6  text-white text-center">
       <div className="rounded overflow-hidden shadow-lg">
         <div className="relative">
-            <img className="w-full" src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Sunset in the mountains" />
-            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25" />
+            <img className="w-full" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Sunset in the mountains" />
+            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-50" />
      
           <a><div className="text-sm absolute top-0 right-0 bg-emerald-500 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3">
               <span className="font-bold">20</span>
@@ -224,9 +224,10 @@ function Blog() {
             </div></a>
         </div>
         <div className="px-6 py-4">
-          <span className="text-lg inline-block text-emerald-500 font-bold">Best Pizza in Town</span>
+          <span className="text-lg inline-block text-emerald-500 font-bold">TECHNOLOGY</span>
+       
           <h3 className="pt-2 text-white">
-            The collection of best pizza images in Newyork city
+            Top Beaches In The World
           </h3>
         </div>
         <div className="px-6 pb-4 flex flex-row items-center">
