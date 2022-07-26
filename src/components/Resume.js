b714e44da08d6ceb33a4138e8e2c144733633c7f
Your Name <you@example.com>
2022-07-26 15:04:37 +0500

    the code is update after I git 5 dolloar project on fiveer

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,21 +1,20 @@
 import React,{useState} from 'react';
+import { BsCardList } from 'react-icons/bs';
+
 
 function Resume() {
 
+  const [isLoading, setIsLoading] = useState(false)
 
-  const [click, setClick] = useState(true);
-
-  function hoverCard( ) {
-    setClick(false);
+  const buttonHandler = () => {
+    setIsLoading(current => !current)
+    console.log(isLoading) // is false 
   }
-  function leaveCard( ) {
-    setClick(true);
-  }
-
-  return <div id='resume' className='h-screen w-full p-4 sm:p-8 lg:p-8 text-white'>
-  <div className={click ? "bg-gray-200 transition duration-1000" : "transition duration-1000 bg-gray-900"} onMouseLeave={leaveCard} onMouseEnter={hoverCard}>be </div>
   
-</div> 
+
+  return <div id='resume'  className=' w-full p-4 sm:p-8 lg:p-8 text-white bg-zinc-800'>
+ <button  onClick={buttonHandler} >Current Status</button>
+</div>
 }
 
 export default Resume;
